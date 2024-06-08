ddcfb24d6abf05d30a3e5f11ec80234dc811813e
Jdbye <jdbye3@gmail.com>
2024-06-08 04:45:04 +0200

    Initial commit

---
+++ ClipboardMonitor.cs
@@ -0,0 +1,136 @@
+﻿using Microsoft.Win32;
+using System.Web;
+using WK.Libraries.SharpClipboardNS;
+
+namespace DropboxDirectLinks
+{
+    internal class ClipboardMonitor : ApplicationContext
+    {
+        readonly static string[] folderPaths = { "/scl/fo/", "/sh/" };
+        readonly static string[] filePaths = { "/scl/fi/", "/s/" };
+        const string startupPath = "DropboxDirectLinks";
+
+        static SharpClipboard clipboard;
+        public ClipboardMonitor()
+        {
+            clipboard = new SharpClipboard();
+
+            clipboard.ObservableFormats.Texts = true;
+            clipboard.ObservableFormats.Files = false;
+            clipboard.ObservableFormats.Images = false;
+            clipboard.ObservableFormats.Others = false;
+
+            clipboard.ClipboardChanged += Clipboard_ClipboardChanged;
+
+            try
+            {
+                var args = Environment.GetCommandLineArgs();
+                if (!args.Any(a => a.EqualsAny(StringComparison.InvariantCultureIgnoreCase, "/noprompt", "-noprompt", "/noautorun", "-noautorun", "/nostartup", "-nostartup")))
+                {
+                    if (!CheckIsStartupApp())
+                    {
+                        if (MessageBox.Show("Add this application to startup?\n\nIf you don't like this prompt, add /noprompt to the launch parameters.", "Add to startup?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                        {
+                            AddToStartup();
+                        }
+                    }
+                }
+            }
+            catch { }
+        }
+
+        private static void Clipboard_ClipboardChanged(object? sender, SharpClipboard.ClipboardChangedEventArgs e)
+        {
+            if (e.ContentType == SharpClipboard.ContentTypes.Text)
+            {
+                string text = e.Content.ToString();
+
+                if (Uri.IsWellFormedUriString(text, UriKind.Absolute))
+                {
+                    Uri uri = new Uri(text);
+                    if (uri.Host.Equals("dropbox.com", StringComparison.InvariantCultureIgnoreCase) || uri.Host.Equals("www.dropbox.com", StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (uri.LocalPath.StartsWithAny(filePaths))
+                        {
+                            var dl = HttpUtility.ParseQueryString(uri.Query).Get("dl"); // Copied dropbox links always have this parameter
+                            if (dl != null)
+                            {
+                                var builder = new UriBuilder(uri);
+                                builder.Query = "raw=1";
+                                Clipboard.SetText(builder.Uri.ToString());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public bool CheckIsStartupApp()
+        {
+            var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            if (key != null)
+            {
+                if (key.GetValue(startupPath) != null)
+                {
+                    key.Close();
+                    return true;
+                }
+                else
+                {
+                    key.Close();
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public bool AddToStartup()
+        {
+            var key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            if (key != null)
+            {
+                key.SetValue(startupPath, System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+                return true;
+            }
+            return false;
+        }
+    }
+
+    public static class StringExtensions
+    {
+        public static bool StartsWithAny(this string stringToCheck, StringComparison? stringComparison = null, params string[] startsWith)
+        {
+            foreach (string s in startsWith)
+            {
+                if (stringComparison == null && stringToCheck.StartsWith(s)) return true;
+                else if (stringComparison != null && stringToCheck.StartsWith(s, (StringComparison)stringComparison)) return true;
+            }
+            return false;
+        }
+        public static bool EqualsAny(this string stringToCheck, StringComparison? stringComparison = null, params string[] startsWith)
+        {
+            foreach (string s in startsWith)
+            {
+                if (stringComparison == null && stringToCheck.Equals(s)) return true;
+                else if (stringComparison != null && stringToCheck.Equals(s, (StringComparison)stringComparison)) return true;
+            }
+            return false;
+        }
+        public static bool StartsWithAny(this string stringToCheck, params string[] startsWith)
+        {
+            foreach (string s in startsWith)
+            {
+                if (stringToCheck.StartsWith(s)) return true;
+            }
+            return false;
+        }
+        public static bool EqualsAny(this string stringToCheck, params string[] startsWith)
+        {
+            foreach (string s in startsWith)
+            {
+                if (stringToCheck.Equals(s)) return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+#Dropbox Direct Links
+Simple concept: Change dropbox links copied to clipboard automatically to direct links. Dropbox allows us to do this thanks to the URI parameter raw=1
+We only want this for files, not folders. raw=1 makes folders download as a zip file. We don't want this. Usually.
+In my testing, these are always folders:
+https://www.dropbox.com/scl/fo/
+https://www.dropbox.com/sh/
+hese are always files:
+https://www.dropbox.com/scl/fi/
+https://www.dropbox.com/s/
+
+It makes sense that "fo" means folder, and "fi" means file. As for the others, who knows what goes.
+Can we assume this will always be the case? Nope.
+Can we assume these are the only types of URLs that will show up? Also nope.
+That is why I rely on your help if you encounter URLs that don't match these patterns.
+The worst that happens if the application gets this wrong, is that your folder is downloaded as a zip archive instead of being viewable in browser.
+I wish the Dropbox client had this feature built in, but it just doesn't.
+There may well be better ways to do this, I just made this to fix an annoyance of mine with the Dropbox client.
+I won't provide a build of this.
+You shouldn't download a prebuilt version of anything that modifies your clipboard contents without asking you.
+I shouldn't need to explain why. And if you don't know why, don't download this repo.
